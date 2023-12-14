6284dfe96dfca0b900187bd0925bc86c20ccace8
David Baden <davebaden@gmail.com>
2023-12-13 18:41:55 -0800

    pass tests

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = f"Hello, my name is {name}."
+        badges.append(badge)
+    return badges
+
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for index, name in enumerate(names, start=1):
+        room = f"Hello, {name}! You'll be assigned to room {index}!"
+        rooms.append(room)
+    return rooms
+
 
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    for message in badge_messages:
+        print(message)
+
+    room_assignments = assign_rooms(names)
+    for assignment in room_assignments:
+        print(assignment)
